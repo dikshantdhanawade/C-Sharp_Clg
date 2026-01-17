a048f077caad29e1e19c13e8485396322c7b50d3
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2026-01-17 11:55:56 +0530

    Add C# programs

---
+++ Arithmatic Operation/Arithmatic Operation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmatic_Operation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 3;
+
+            Console.WriteLine("Addition: " + (a + b));
+            Console.WriteLine("Subtraction: " + (a - b));
+            Console.WriteLine("Multiplication: " + (a * b));
+            Console.WriteLine("Division: " + (a / b));
+            Console.WriteLine("Modulus: " + (a % b));
+        }
+    }
+}
---
+++ Even And Odd/Even And Odd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_And_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Enter an Number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0) { 
+                Console.WriteLine("{0} is Even Number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Odd Number", num);
+            }
+        }
+    }
+}
