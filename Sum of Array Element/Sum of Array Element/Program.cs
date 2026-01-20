65d29e6e31db9dbe755865004be8c66ab427f306
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2026-01-20 15:19:11 +0530

    commit

---
+++ Sum of Array Element/Sum of Array Element/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Sum_of_Array_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements in the array:");
+            int arrLength = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[arrLength];   
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.WriteLine("Enter element {0}:", i + 1);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int sum = 0;
+            for (int i = 0; i < arrLength; i++)
+            {
+                sum += arr[i];
+            }
+
+            Console.WriteLine("Sum of array elements: " + sum);
+        }
+    }
+}
---
+++ fctorial of a number/fctorial of a number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fctorial_of_a_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to calculate its factorial:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            long factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine($"The factorial of {number} is {factorial}");
+            Console.ReadLine();
+        }
+    }
+}
