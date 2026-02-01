dab10981b642a42cb5dd02513c591b8df0701384
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2026-02-01 11:14:12 +0530

    commit

---
+++ Accesmodifier/Accesmodifier/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//private access modifier example
+class car
+{
+    private string model = "mustang";
+    
+        static void Main(string[] args)
+        {
+        car Obj = new car();
+        Console.WriteLine(Obj.model);
+    }
+    }
+
+//public access modifier example
+
+class bike
+{
+    public string model = "BMW";
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bike myObj = new bike();
+        Console.WriteLine(myObj.model);
+    }
+}
+
---
+++ Accesmodifier/Accesmodifier/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Accesmodifier")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Accesmodifier")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b3c37c6e-3be5-43a0-b1f1-f4f33273e6d8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
